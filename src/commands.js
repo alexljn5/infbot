8761a0b7d6af85d327fa4d34860d51d86fe4ce3b
alexljn5 <alexljn5@gmail.com>
2026-03-14 11:30:29 +0100

    neat
    Returns random Cream  images

--- src/commands.js
+++ src/commands.js
@@ -1,8 +1,11 @@
+const { EmbedBuilder } = require('discord.js');
+const { getRandomCreamImage } = require('./network/cream_net_fetch');
+
 module.exports = {
     hello: {
         description: 'Replies with glitchy bnuuy!',
         execute: (message) => {
-            message.reply('b̴̨̢͔͚̊̽͛̽͋̒̔̈́̄̍̾̚͜͠ņ̶̡̧̡̢̧̧̨̢̨̢̡̢̛̛͖͓̤̟̦̝͚̫̖͓͖̣̱̭̣̩͓͉̭̫̭͓͇͈̖̖̺͈͉̪̘͍͔̝̠͕̜̥͇̞̘̹̰̦̦̩̳̪̹͔̘̤̼̗͕͇̭̘͎͓̤̳̟͍̲͉̞͓̲̞̪̟̞͕̥̼͙͎͉͔̜̖͓̝̩͙̙͈̪̯̥̦̙̦͎̖̫̯̭̙̹̠̖̼̭͖͓̗̩̖̘̙̲̤̥͇̲̬͛̍̉̐͛͑͛̊̄̔͐͋̀̍̑́͗͋͌͂͌̈́̃̓̎̏͗̄̍̊̍͗͗̔͒̌̉̄͛̓͗̄̊͋̆̔̈́͐͌̀̏̋̓͐̈́̍̈́̂̋͑̽͗̐͗̏͋́̉̍͂̀̊͌̈́͂͛̔̇̈̓̆̈́̊̀̃͊̓̄̋͗̈̋̄̈́͗͆̔̇͒̂̈́̐̆̄̿̏͑̌͘̚̕͜͜͜͜͜͜͜͝͝͝͝͠͝͠͠͠͠ͅư̸̡̢̨̡̢̨̡̢̢̧̛̛̛̛̖̞̼̗̙̲̗͎͙̯̹͙̥̞̦̘̟̤͎͈̱̰̝͚̞̘̘̩̥̩̠͈̘̦̦̖̜̦̘̖̭̝̤̺̘͕͚̳̝͓̤͓̟͓̖̙̥̗̝͔͉̙̗͔͈̠͈̮̮͖͈̟̝̰̝͇̹̯̳̯͇̝͎͕̬̣̞͙͔̬̜̪͙̲̼̻͇̲̯͕̝͔̗̳̼͖̳̝̠̥͉̘͇̩̗͙̘̪̺̹͚͈͙̳̤̬͈̫̬͖̺̝̖̞̺̹͖̼͉̫͙̅̐̀͐͆͗̐̉̀̓̈͆̏̓̔̌̀̂͆̇̋͂̍͂̊͋̈́̈́̓͆̀̀̐̎̅̿́̈̌̈́̌͐̀̏͑͐̓̾͐̉́̇̐̀̽̊̀̅̂̅̆̄̾̐̂̅̋̑͛͆̓̾̓͛̌̒͂́͗͒̄̎̓̆̌̈́͐́̈́̓͐̎͐̽̀͊̏̋̋̇̓̈́̀̾̄͂̈́̑̒̊̀̓̽̅̓̈͋̀͛̏̅̈́̋̆̆͑̓̽̌͊̇̏̀̑̈́̅̉͒͒̏̾̍̈́̌͆̋͗́͐̈́͐̃͆̅̍͊́͂̆͌̈́̔̈̾̓͆͌̔̋́̅̌͂̽̿̎̿͛̒̑͐͂͋̐͋͆̿̀̅͊͂̅́́͛̽̓̒̅͌͑͆̉̓͋̔͂̓̒̊̍͂̈̂̈́̔̓̌̓̑́̅̔̀́́̌̂̌̏̿̊̏̐̽̈́̐̌̑̔̑̈́͛͐̚̕͘̕͘͘͘͘̚̚̚͘̕̕̕̕̚̕͜͜͜͜͜͜͠͝͝͝͠͝͝͠͝͝͠͝͝͝͝͝ͅͅͅư̷̧̨̢̧̡̡̢̧̧̡̛͔̼̰̙̩̟͈̙̤͇̗̙̹̪̝̻͉̗̟̘̥̝͕̪̰̬̳̰͚̘͇̲̳͍̜̼̜̞̪̠̹͇͚̺̞̥̗̜̞̪͔̲̞̻̪̞̞̯̪̤̰̪̟͉͍͚̠̭̜̗̯̦̙̞͇̹̲̭̱̱͙̦͚̖͈̝͖̱͎̥̰͔̺̮͉̞̙̦̮̼̺̬̲̬͍̳̼͈̠̩̞͕̳̙͚̟͙̬͉͚̲̟̬̤͍̟̺̐͗̋̀́́̆̿̾͛̈́̐̒̑͐̀̎̄͛̈́͐́̈̂̀̊͛̏͆̅̀̈̈́̔́̈́͒̓͒̂̀͒̔̿̎͌́͋̓͛̄̋̄̏̌́͊̾̑̇̓͆̒̀̆̊͆̽͋́̑͆̇̑̎̊̐̇͛̀͐̈́͑̈̏̆̒̔̎̉̒͒̾̎̈́̊̀̀̋̉̓̍̆̈͊̒̃̒̄͑͂̊̉̈́̎̓̌̋͋̈́͒̇̾̓̓̊̓̉̒̄̈͐̒͗́͋́̌̍̀̄̇͑̾̑̌͋̾̕͘̕̕͘̚̚̕͘̚̚̚̚̚͜͝͝͝͝͝͝ͅͅỷ̷̴̧̧̢̨̢̡̨̢̡̢̧̧̨̢̨̢̡̢̛̫͓̻̱̼̦̪̯̠͈̬͚̥͎͔̞̝̗̣͙̠̪͉̰͈̳̭̟̬̪̳̞͚̝̝͙͇͕̬͉̤͇̫̪̯̝͍̮̼̩̞̤͎̥̖͚̩̼͕̯̜̝͉̼͓͚̘̞͍̦̖̗̱͚̻̣̰͈͓̠͓͙͚̹̩̺̗̙̰̟͈̻̮̲̗̣̣͕̻̖͚͈̟̜̙̭͇̩̩̮̟̗̬̙̜̱̟̭̜̬̲̰͔̤̥̥̘̦̘̝̥͈͓͈̮̜̣̳̩̟̱̙͎̮̹̭̫͍̞͍̼̗͎͎͚̞̫̖̘̩͎̠̲͎̻̬̹̰̦̦̩̳̪̹͔̘̤̼̗͕͇̭̘͎͓̤̳̟͍̲͉̞͓̲̞̪̟̞͕̥̼͙͎͉͔̜̖͓̝̩͙̙͈̪̯̥̦̙̦͎̖̫̯̭̙̹̠̖̼̭͖͓̗̩̖̘̙̲̤̥͇̲̬́̏͋̂͗̓̄̀́͒͛̏͐͂̈̐̓̇́͌͑̌̽͛̓̏̋̒̑̋͒̌͋͗̏̅̈̾̋͑̓̉͒̈̄̍̄̑̈́̑̓̍̈́͑̊̌̋̀̑̃̿̀͆̊̑̇̀̓̑̒̏̎̕̚̚͘͘̕̚͘̚͜͜͜͜͜͜͜͜͜͜͝͠͠͝͝ͅͅͅͅͅͅư̸̡̢̨̡̢̨̡̢̢̧̛̛̛̛̖̞̼̗̙̲̗͎͙̯̹͙̥̞̦̘̟̤͎͈̱̰̝͚̞̘̘̩̥̩̠͈̘̦̦̖̜̦̘̖̭̝̤̺̘͕͚̳̝͓̤͓̟͓̖̙̥̗̝͔͉̙̗͔͈̠͈̮̮͖͈̟̝̰̝͇̹̯̳̯͇̝͎͕̬̣̞͙͔̬̜̪͙̲̼̻͇̲̯͕̝͔̗̳̼͖̳̝̠̥͉̘͇̩̗͙̘̪̺̹͚͈͙̳̤̬͈̫̬͖̺̝̖̞̺̹͖̼͉̫͙̅̐̀͐͆͗̐̉̀̓̈͆̏̓̔̌̀̂͆̇̋͂̍͂̊͋̈́̈́̓͆̀̀̐̎̅̿́̈̌̈́̌͐̀̏͑͐̓̾͐̉́̇̐̀̽̊̀̅̂̅̆̄̾̐̂̅̋̑͛͆̓̾̓͛̌̒͂́͗͒̄̎̓̆̌̈́͐́̈́̓͐̎͐̽̀͊̏̋̋̇̓̈́̀̾̄͂̈́̑̒̊̀̓̽̅̓̈͋̀͛̏̅̈́̋̆̆͑̓̽̌͊̇̏̀̑̈́̅̉͒͒̏̾̍̈́̌͆̋͗́͐̈́͐̃͆̅̍͊́͂̆͌̈́̔̈̾̓͆͌̔̋́̅̌͂̽̿̎̿͛̒̑͐͂͋̐͋͆̿̀̅͊͂̅́́͛̽̓̒̅͌͑͆̉̓͋̔͂̓̒̊̍͂̈̂̈́̔̓̌̓̑́̅̔̀́́̌̂̌̏̿̊̏̐̽̈́̐̌̑̔̑̈́͛͐̚̕͘̕͘͘͘͘̚̚̚͘̕̕̕̕̚̕͜͜͜͜͜͜͠͝͝͝͠͝͝͠͝͝͠͝͝͝͝͝ͅͅͅư̷̧̨̢̧̡̡̢̧̧̡̛͔̼̰̙̩̟͈̙̤͇̗̙̹̪̝̻͉̗̟̘̥̝͕̪̰̬̳̰͚̘͇̲̳͍̜̼̜̞̪̠̹͇͚̺̞̥̗̜̞̪͔̲̞̻̪̞̞̯̪̤̰̪̟͉͍͚̠̭̜̗̯̦̙̞͇̹̲̭̱̱͙̦͚̖͈̝͖̱͎̥̰͔̺̮͉̞̙̦̮̼̺̬̲̬͍̳̼͈̠̩̞͕̳̙͚̟͙̬͉͚̲̟̬̤͍̟̺̐͗̋̀́́̆̿̾͛̈́̐̒̑͐̀̎̄͛̈́͐́̈̂̀̊͛̏͆̅̀̈̈́̔́̈́͒̓͒̂̀͒̔̿̎͌́͋̓͛̄̋̄̏̌́͊̾̑̇̓͆̒̀̆̊͆̽͋́̑͆̇̑̎̊̐̇͛̀͐̈́͑̈̏̆̒̔̎̉̒͒̾̎̈́̊̀̀̋̉̓̍̆̈͊̒̃̒̄͑͂̊̉̈́̎̓̌̋͋̈́͒̇̾̓̓̊̓̉̒̄̈͐̒͗́͋́̌̍̀̄̇͑̾̑̌͋̾̕͘̕̕͘̚̚̕͘̚̚̚̚̚͜͝͝͝͝͝͝ͅͅỷ̷̧̧̢̨̢̡̨̢̨̨̨̨̡̛̫͓̻̱̼̦̪̯̠͈̬͚̥͎͔̞̝̗̣͙̠̪͉̰͈̳̭̟̬̪̳̞͚̝̝͙͇͕̬͉̤͇̫̪̯̝͍̮̼̩̞̤͎̥̖͚̩̼͕̯̜̝͉̼͓͚̘̞͍̦̖̗̱͚̻̣̰͈͓̠͓͙͚̹̩̺̗̙̰̟͈̻̮̲̗̣̣͕̻̖͚͈̟̜̙̭͇̩̩̮̟̗̬̙̜̱̟̭̜̬̲̰͔̤̥̥̘̦̘̝̥͈͓͈̮̜̣̳̩̟̱̙͎̮̹̭̫͍̞͍̼̗͎͎͚̞̫̖̘̩͎̠̲͎̻̬̹̜̠̣̞͔̼̠̙̟̻̼̫̥͈̜̬͍̜͉̺̫̲̲͈̟̹̤̺̬̮͇̙̹̱̞̞̥͙̣̘̟̼́̏͋̂͗̓̄̀́͒͛̏͐͂̈̐̓̇́͌͑̌̽͛̓̏̋̒̑̋͒̌͋͗̏̅̈̾̋͑̓̉͒̈̄̍̄̑̈́̑̓̍̈́͑̊̌̋̀̑̃̿̀͆̊̑̇̀̓̑̒̏̎̕̚̚͘͘̕̚͘̚͜͜͜͝͠͠͝͝ͅͅͅͅͅͅ there!');
+            message.reply('bnuuy!');
         }
     },
     roll: {
@@ -24,17 +27,27 @@ module.exports = {
             const fs = require('fs');
             const path = require('path');
             const dir = path.join(__dirname, 'ascii');
-            const files = fs.readdirSync(dir).filter(f => /^cat\d+\.txt$/.test(f));
+            let files;
+            try {
+                files = fs.readdirSync(dir).filter(f => /^cat\d+\.txt$/.test(f));
+            } catch {
+                return message.reply('No ascii dir!');
+            }
             if (files.length === 0) return message.reply('No cat files!');
             const randomFile = files[Math.floor(Math.random() * files.length)];
-            const cat = fs.readFileSync(path.join(dir, randomFile), 'utf8').trim();
+            let cat;
+            try {
+                cat = fs.readFileSync(path.join(dir, randomFile), 'utf8').trim();
+            } catch {
+                return message.reply('Error reading cat!');
+            }
             message.reply(`\`\`\`\n${cat}\n\`\`\``);
         }
     },
     meme: {
         description: 'A cool ass meme',
         execute: (message) => {
-            message.reply('W̸h̷e̴n̶ ̷t̴h̶e̷ ̴c̵o̴d̴e̷ ̷w̴o̴r̷k̴s̴ ̴b̵u̶t̷ ̴y̷o̴u̷ ̴d̶o̶n\'t̴ ̴k̴n̵o̴w̴ ̴w̵h̷y̴:̶ ̴b̶r̷u̴h̶');
+            message.reply('When the code works but you don\'t know why: bruh');
         }
     },
     reverse: {
@@ -50,9 +63,24 @@ module.exports = {
         execute: (message) => {
             const chars = '\u030d\u030e\u0304\u0305\u033f\u0311\u0306\u0310\u0352\u0357\u0351\u0307\u0308\u0303\u0302\u030a\u0348\u0341\u0344\u034a\u034b\u034c'.split('');
             const text = message.content.slice(6).trim() || 'zalgo';
-            const zalgoed = text.split('').map(c => c + Array.from({ length: ~~(Math.random() * 7) }, () => chars[~~(Math.random() * chars.length)]).join('')).join('');
+            const zalgoed = [...text].map(c => c + Array.from({ length: Math.floor(Math.random() * 7) }, () => chars[Math.floor(Math.random() * chars.length)]).join('')).join('');
             message.reply(zalgoed);
+        }
+    },
+    cream: {
+        description: '.cream - Random Cream the Rabbit image',
+        execute: async (message) => {
+            try {
+                const img = await getRandomCreamImage();
+                const embed = new EmbedBuilder()
+                    .setTitle('Cream the Rabbit!')
+                    .setImage(img)
+                    .setColor('#FFB6C1');
+                await message.channel.send({ embeds: [embed] });
+            } catch (err) {
+                console.error(err);
+                message.reply('Error fetching Cream image!');
+            }
         }
     }
 };
-
